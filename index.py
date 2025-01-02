69d5f9a28cdf0f46650ae1f414fe4908456739ad
shahidasamrin <shahidasamrinshaik@gmail.com>
2025-01-02 22:03:34 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,66 @@
+[
+# print("Hello World")
+# print("###")
+
+# Addition
+
+# Digit_one = 2495
+# Digit_Two = 789358
+# print(Digit_one + Digit_Two)
+
+# Substraction
+# Digit_One = 596
+# Digit_Two = 193856
+# print(Digit_Two - Digit_One)
+
+
+# Product
+# Digit_One = 37
+# Digit_Two = 61
+# Digit_Three = 391
+# print(Digit_One * Digit_Two * Digit_Three)
+
+# Divisible
+# Digit_One = 33968
+# Digit_Two = 176
+# print(Digit_One / Digit_Two)
+
+# a = int(input("Please Enter a Number One "))
+# b = int(input("Please Enter a Number Two "))
+# result = a + b
+# print("Sum : " + str(result))
+
+# Rectangle l * W
+
+# Length_of_Rectangle = int(input("Enter Length of Rectangle : "))
+# Width_of_Rectangle = int(input("Enter Width of Rectangle : "))
+# Area_of_Rectangle = Length_of_Rectangle * Width_of_Rectangle
+# print(Area_of_Rectangle)
+
+# Square l * W
+
+# length_of_Square = int(input("Enter a Length of Square : "))
+# Width_of_Square = int(input("Enter a Width of Square : "))
+# Area_of_Square = length_of_Square * Width_of_Square
+# print(Area_of_Square)
+
+# Circle pie r^2
+
+# Radius_of_Circle = int(input())
+# Area_of_Circle = 3.14 * Radius_of_Circle ** 2
+# print(Area_of_Circle)
+
+# Triangle 1/2* (b * h)
+
+# Breath_of_Triangle = int(input("Enter a Breath of Triangle: "))
+# height_of_Triangle = int(input("Enter a height of Triangle: "))
+# Area_of_Triangle = 1/2 * Breath_of_Triangle * height_of_Triangle
+# print(Area_of_Triangle)
+
+# Parallelogram  b * h
+
+# Breath_of_Parallelogram = int(input("Enter a Breath Number: "))
+# Height_of_Parallelogram = int(input("Enter a Height Number: "))
+# Area_of_Parallellogram = Breath_of_Parallelogram * Height_of_Parallelogram
+# print(Area_of_Parallellogram)
+]
